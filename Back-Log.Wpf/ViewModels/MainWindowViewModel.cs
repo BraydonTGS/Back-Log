d9c468e53d996d40ff52bd5d14965b54408f3205
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-10 17:42:59 -0500

    Created MainViewModel

--- Back-Log.Wpf/ViewModels/MainWindowViewModel.cs
+++ Back-Log.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,46 @@
-﻿namespace Back_Log.Wpf.ViewModels
+﻿using Back_Log.Wpf.Base;
+using Prism.Commands;
+using System;
+
+namespace Back_Log.Wpf.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                SetProperty(ref _username, value);
+            }
+        }
+
+        public DelegateCommand MinimizeWindowCommand { get; set; }
+        public DelegateCommand CloseWindowCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Title = "Back-Log";
+
+            MinimizeWindowCommand = new DelegateCommand(OnMinimizeWindowCommandExecuted);
+            CloseWindowCommand = new DelegateCommand(OnCloseWindowCommandExecuted);
+
+            Username = Environment.UserDomainName;
+
+        }
+
+        #region Commands
+        private void OnCloseWindowCommandExecuted()
+        {
+            App.Current.MainWindow.Close();
+        }
+
+        private void OnMinimizeWindowCommandExecuted()
+        {
+            App.Current.MainWindow.WindowState = System.Windows.WindowState.Minimized;
+        }
+        #endregion
     }
 }
