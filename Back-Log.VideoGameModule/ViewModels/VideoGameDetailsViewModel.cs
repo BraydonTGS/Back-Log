90d5f12dbb20415626f64f3789e9624cf37d89e1
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-10 18:21:54 -0500

    Created VideoGameDetails View and ViewModel

---
+++ Back-Log.VideoGameModule/ViewModels/VideoGameDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Back_Log.Wpf.Base;
+
+namespace Back_Log.VideoGameModule.ViewModels
+{
+    public class VideoGameDetailsViewModel : ViewModelBase
+    {
+    }
+}
---
+++ Back-Log.VideoGameModule/Views/VideoGameDetails.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Back_Log.VideoGameModule.Views
+{
+    /// <summary>
+    /// Interaction logic for VideoGameDetails.xaml
+    /// </summary>
+    public partial class VideoGameDetails : UserControl
+    {
+        public VideoGameDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
