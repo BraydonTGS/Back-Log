053c7f67e217b1a61f3e4af390474a0753cf9235
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-10 17:39:21 -0500

    Installed and Set up WPF to run with Prism

---
+++ Back-Log.Wpf/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Back_Log.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
