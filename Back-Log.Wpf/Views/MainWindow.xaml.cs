24729bf9dbddd81ffc2fc949b8737ef3d380165a
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-10 17:52:44 -0500

    Added Style to Main Window

--- Back-Log.Wpf/Views/MainWindow.xaml.cs
+++ Back-Log.Wpf/Views/MainWindow.xaml.cs
@@ -11,5 +11,15 @@ namespace Back_Log.Wpf.Views
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Allows you to Drag and Drop the Window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
     }
 }
