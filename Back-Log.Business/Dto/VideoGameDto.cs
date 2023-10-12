b9ea1a9d064ace54b9868b2a26b8b446186c32ca
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-12 17:34:38 -0500

    Created a Simple VideoGameDto

---
+++ Back-Log.Business/Dto/VideoGameDto.cs
@@ -0,0 +1,13 @@
+﻿using Back_Log.Global.Constants;
+
+namespace Back_Log.Business.Dto
+{
+    public class VideoGameDto
+    {
+        public string Title { get; set; }
+        public Enums.VideoGameGenre GameGenre { get; set; }
+        public Enums.ESRBRating ESRBRating { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
