3f566b6e1672b42a521b1f8fd299948b39e78e11
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2023-10-12 17:44:57 -0500

    Created VideoGameId DtoModelBase

---
+++ Back-Log.Business/Base/DtoModelBase.cs
@@ -0,0 +1,16 @@
+﻿namespace Back_Log.Business.Base
+{
+    public class DtoModelBase
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public DtoModelBase()
+        {
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            IsDeleted = false;
+        }
+    }
+}
--- Back-Log.Business/Dto/VideoGameDto.cs
+++ Back-Log.Business/Dto/VideoGameDto.cs
@@ -1,13 +1,18 @@
 ﻿using Back_Log.Global.Constants;
+using Back_Log.Global.Identifiers;
 
 namespace Back_Log.Business.Dto
 {
     public class VideoGameDto
-    {
+    {      
+        public VideoGameId VideoGameId { get; private set; }
         public string Title { get; set; }
         public Enums.VideoGameGenre GameGenre { get; set; }
         public Enums.ESRBRating ESRBRating { get; set; }
         public bool IsCompleted { get; set; }
-
+        public VideoGameDto()
+        {
+          VideoGameId = new VideoGameId(Guid.NewGuid());
+        }
     }
 }
